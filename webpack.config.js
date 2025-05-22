1b63019b53f750faf72d2a219fd3a07a1e3991a0
npitchforth <npitchforth@gmail.com>
2025-05-22 18:40:58 +1000

    refactor: improve webpack configuration with environment-based settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,69 +2,74 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react']
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+      clean: true // This replaces the rm -rf dist command
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
           }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-      inject: true
-    }),
-    new CopyPlugin({
-      patterns: [
-        { 
-          from: 'public',
-          to: '.',
-          globOptions: {
-            ignore: ['**/index.html']
-          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
       ]
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, '/'),
     },
-    port: 3001,
-    hot: false,
-    liveReload: false,
-    historyApiFallback: true,
-    proxy: {
-      '/flags': {
-        target: 'https://flagcdn.com',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/flags': ''
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+        inject: true
+      }),
+      new CopyPlugin({
+        patterns: [
+          { 
+            from: 'public',
+            to: '.',
+            globOptions: {
+              ignore: ['**/index.html']
+            }
+          }
+        ]
+      })
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, '/'),
+      },
+      port: 3001,
+      hot: false,
+      liveReload: false,
+      historyApiFallback: true,
+      proxy: {
+        '/flags': {
+          target: 'https://flagcdn.com',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/flags': ''
+          }
         }
       }
     }
-  }
-}; 
+  };
+};
