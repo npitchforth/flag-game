37f94e3a54346092d235efedd3fa70f3650dbbb6
npitchforth <npitchforth@gmail.com>
2025-06-23 17:06:18 +1000

    fix: restore all local files and reset git index after accidental staged deletions

--- src/config/difficultySettings.js
+++ src/config/difficultySettings.js
@@ -3,21 +3,21 @@
 
 export const difficultySettings = {
   easy: { 
-    time: 45, 
+    time: 13, 
     optionCount: 4,
     streakThreshold: 5, // Answers needed for a streak bonus
     streakTimeBonus: 3,   // Seconds to add for a bonus
     maxStreakBonuses: 3, // Max number of times bonus can be awarded
   },
   medium: { 
-    time: 45, 
+    time: 13, 
     optionCount: 4,
     streakThreshold: 5,
     streakTimeBonus: 3,
     maxStreakBonuses: 3,
   },
   hard: { 
-    time: 45, 
+    time: 13, 
     optionCount: 4,
     streakThreshold: 5,
     streakTimeBonus: 5,
